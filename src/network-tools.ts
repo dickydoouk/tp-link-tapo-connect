739aeadae6e8edf96cf53cf671164f768a1e3408
Paul Dickinson <paul.dickinson@anaplan.com>
2024-02-22 22:55:07 +0000

    bump 2.0.2

--- src/network-tools.ts
+++ src/network-tools.ts
@@ -1,8 +1,10 @@
 import { parse } from 'macaddr';
 import arp from '@network-utils/arp-lookup'
 
-export const resolveMacToIp = async (mac: string) :Promise<string> => {
-    return arp.toIP(tidyMac(mac));
+export const resolveMacToIp = async (mac: string) :Promise<string | null> => {
+    const mac2 = tidyMac(mac);
+    const ip = await arp.toIP(mac2);
+    return ip;
 }
 
 const tidyMac = (mac: string): string => 
