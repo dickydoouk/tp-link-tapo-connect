f17cc703d7b929df3cf41937d0a2b31f71e257bc
Vachter Roland <roland.vachter@nagarro.com>
2024-11-06 11:24:31 +0200

    Arp lookup sometimes fails at first and needs several trials to succeed.

--- src/network-tools.ts
+++ src/network-tools.ts
@@ -4,7 +4,15 @@ import find from 'local-devices';
 
 export const resolveMacToIp = async (mac: string) :Promise<string | null> => {
     const mac2 = tidyMac(mac);
-    return await arp.toIP(mac2) ?? (await find()).find(devices => devices.mac === mac2)?.ip;
+    let trial = 0;
+    let ip = await arp.toIP(mac2) ?? (await find()).find(devices => devices.mac === mac2)?.ip;
+
+    while (trial < 5 && !ip) {
+        trial++;
+        ip = await arp.toIP(mac2) ?? (await find()).find(devices => devices.mac === mac2)?.ip;
+    }
+
+    return ip;
 }
 
 const tidyMac = (mac: string): string => 
